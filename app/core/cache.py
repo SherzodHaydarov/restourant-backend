2791e1d8dd596e7bb752a9ae9bf1cf20b0b763bd
Sherzod Haydarov <sherzodkhaydarov0888@gmail.com>
2026-03-27 22:47:20 +0500

    cache.py o'zgarish kiritildi

--- app/core/cache.py
+++ app/core/cache.py
@@ -35,7 +35,7 @@ async def close_redis():
     """Close Redis connection"""
     global redis_client
     if redis_client:
-        await redis_client.aclose()
+        await redis_client.close()
         logger.info("Redis connection closed")
 
 
